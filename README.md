4906a6765ad32e0057e0883a506812153c832839
Su Han <126628180+su-gis@users.noreply.github.com>
2025-01-21 10:51:38 -0600

    Commit
    Commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Census and ACS data collection & preprocessing
+Python programs to help census or acs data collection, aggregation, normalization at both tract and count level. Initially we made the python programs with ACS 2018 5 year estimate, but the codes should be resuable for both census and acs data in different years.
+
+### Section1: Download tract-level data by using the Census API for all states in the U.S.
+- section 1\Data_collection_cbg_tract_ByState\Tract_Join_to_Shp_California_BlockGroup_2023.ipynb how census block group level and tract level ACS data in a particular state were collected and preprocessed.
+
+- The output of Section1 is the input of Section2
+- The description of each code is available here: https://api.census.gov/data/2018/acs/acs5/variables.html
+
+- ##### Websites related to CensusAPI
+    - https://www.census.gov/data/developers/data-sets.html
+    - https://www.census.gov/data/developers/data-sets/acs-5year.html
+	- https://api.census.gov/data/2018/acs/acs5/examples.html
+
+
+### Section2: Data selection, aggregation, normalization at both tract and county level. Zipcode level is also supported
+#### ACS_selection_tract.py and ACS_selection_zipcode.py
+
+- ###### How to run examples:
+       python ACS_selection_blockgroup.py -i data/ACS_2023_5year__cbg_CA.csv -c data/ACS_2023_5year_codebook_all.xlsx -o data/output_California
+	   
+	   python ACS_selection_tract.py -i data/ACS_2023_5year__tract_CA.csv -c data/ACS_2023_5year_codebook_all.xlsx -o data/output_California
+
+
+ - -i  input: Provide the combined ACS data file downloaded in the previous step. This file is the output generated during the earlier process in the folder 'section 1'. 
+ - -c input: This is the control file. In other words, the data you entered above as a parameter -i,  will be selected or calculated based on the rules in this file.
+
+   - colA (CODE) - Required : In order to see the variables in your output file, you need to list it in this column. If you do not list the variable CODE in colA, it will not be printed in the output file. In addition, you need to enter “1” in the colB (use) to print the variable in your outputfile. When you enter multiple variable code in colA, it will sum up the variables.For example, B27011_004E, B27011_009E, B27011_014E → it means all variables will be summed up like this: B27011_004E + B27011_009E + B27011_014E 
+   - colB (use) - Required: You need to enter “1” in the colB (use) to print the variable in your outputfile. If colB is empty, the variable will not be printed in the output file. 
+  
+   - colC (denominator) - Optional: enter the variable code that you want to use to normalize the variable in ColA. If colC is empty, it will use the count without the normalization. For example, for the 3rd in ACS_2018_5year_selected_codebook.xlsx, B06009_004E in col A is divided by B06009_001E and multiplied by 100 in your outputfile. For the 20th row in the same file,
+ ((B27011_004E + B27011_009E + B27011_014E) / B27011_001E )*100
+   - ColD (Original Name) - Optional: long name of the variables. It will not be shown in your output file.
+   - ColE (Short Name) - Required: it will be displayed as column headers in one of your output files (the one for count).
+   - ColF (Name_normalized) - Optional: it will be displayed as column headers in one of the output files (the one for normalized data).
+   - ColG (GROUP) - Optional: This column is just for your reference and is not used for computation.
+
+  -  -o  output: Enter the folder name. Enter the folder name that exists. 4 csv files will be created
+ -     # output of ACS_selection_blockgroup.py.py
+        1. XXXX_byTract_summarized.csv: cout in block group level
+        2. XXXX_byTract_normalized.csv: Normalized (percentage) in  block group level
+		
+  -     # output of ACS_selection_tract.py
+        1. XXXX_byTract_summarized.csv: cout in tract level
+        2. XXXX_byTract_normalized.csv: Normalized (percentage) in tract level
+
+  -     # output of ACS_selection_county.py
+        1. XXXX_byTract_summarized.csv: cout in county level
+        2. XXXX_byTract_normalized.csv: Normalized (percentage) in county level		
+
+  -     # output of ACS_selection_zipcode.py
+        1. XXXXL_byZipcode_summarized.csv: cout in zipcode level
+        2. XXXX_byZipcode_normalized.csv: Normalized (percentage) in zipcode level
+		
+     
+- No data value in your output file will be displayed as -9999
+-------------
+If you have questions, please contact Dr. Su Yeon Han at su.han@txstate.edu
---
+++ section 2/.ipynb_checkpoints/ACS_selection_blockgroup-checkpoint.py
@@ -0,0 +1,203 @@
+import sys, getopt
+#import csv
+import datetime
+from datetime import datetime
+import pandas as pd
+from pytz import timezone
+import pytz
+import json
+import math
+
+# Enter  your desidred no data value in your output
+NO_DATA_VALUE = -9999
+
+
+def main(inputFile, codebook, ouputDir):
+    
+    df_input = pd.read_csv(inputFile)
+    df_codebook = pd.read_excel(codebook)
+    #print(df_input)
+    #print(df_codebook)
+    
+    outputFile = inputFile.split('/')[-1]        # get a file name from the inputFile full path
+    p = outputFile.rfind('.')                    # get start position of the file surfix
+    if (p > 1): outputFile = outputFile[:p]      # delete the file surfix
+    
+    CBGIDs = []
+    for i in range(len(df_input)):
+        #geoid = "{:02d}{:05d}".format(df_input.loc[i, 'state'], df_input.loc[i, 'zipcode'])    
+        geoid = "{:02d}{:03d}{:06d}{:01d}".format(df_input.loc[i, 'state'], df_input.loc[i, 'county'], df_input.loc[i, 'tract'], df_input.loc[i, 'block group'])
+        #if (len(geoid) != 7):        
+        if (len(geoid) != 12): 
+            print("Invalid length of CBGid in {}    line-no: {} [{}]  {} bytes ".format(inputFile, i, geoid, len(geoid)))
+        CBGIDs.append(geoid)
+    df_byCBG = df_input.copy(deep=True)
+    df_byCBG.insert(loc=0, column='geoid', value=CBGIDs)
+    #print(df_byCBG)
+    create_output(df_byCBG, df_codebook, ouputDir+"/"+outputFile+"_byCBG")
+    '''
+    # create summary of df_input by county
+    dataSP = 3                                   # Data Start Position in the columns of df_input
+    summaryDict = {}                             # key: countyID, value: summary of df_input (array)
+    summaryHeader = ["geoid", "nCBGs", "dummy"] + list(df_input.columns)[dataSP:]
+    #print(summaryHeader)
+    for i in range(len(df_input)):
+        #if (i > 21): break
+        geoid = "{:02d}{:03d}".format(df_input.loc[i, 'state'], df_input.loc[i, 'county'])
+        if (len(geoid) != 5): 
+            print("Invalid length of countyid in {}    line-no: {} [{}]  {} bytes ".format(inputFile, i, geoid, len(geoid)))
+        if (geoid in summaryDict):
+            values = summaryDict[geoid]
+            values[1] += 1
+            for j in range(dataSP, len(list(df_input.columns))):
+                if (math.isnan(df_input.iat[i, j]) or df_input.iat[i, j] == -666666666): 
+                    continue
+                if (values[j] == -666666666): values[j] = df_input.iat[i, j]
+                else:                         values[j] += df_input.iat[i, j]
+            summaryDict[geoid] = values
+        else:
+            values = [geoid, 1, ""] + [-666666666]*(len(list(df_input.columns))-dataSP)
+            for j in range(dataSP, len(list(df_input.columns))):
+                if (math.isnan(df_input.iat[i, j]) or df_input.iat[i, j] == -666666666): 
+                    continue
+                values[j] = df_input.iat[i, j]
+            summaryDict[geoid] = values
+    #print(summaryDict)
+    summaryList = list(summaryDict.values())
+    summaryList = sorted(summaryList, key=lambda k: k[0])
+    #print(summaryList)
+    df_byCounty = pd.DataFrame(columns=summaryHeader, data=summaryList)
+    #print(df_byCounty)
+    create_output(df_byCounty, df_codebook, ouputDir+"/"+outputFile+"_byCounty")
+    '''
+    return
+
+ 
+def create_output(df_input, df_codebook, outputFile):
+    
+    header_summarized = ["GEOID"]
+    header_normalized = ["GEOID"]
+    for j in range(len(df_codebook)):
+        if (df_codebook.loc[j, 'use'] != 1): continue
+        if isinstance(df_codebook.loc[j, 'CODE'], float): continue
+        header_summarized.append(df_codebook.loc[j, 'Short_name'])
+        header_normalized.append(df_codebook.loc[j, 'Name_normalized'])
+    #print(len(header_summarized), header_summarized)
+    #print(len(header_normalized), header_normalized)
+    
+    list_summarized = []
+    list_normalized = []
+    for i in range(len(df_input)):
+        #if (i > 0): break
+        geoid = df_input.loc[i, 'geoid']
+        aSeries = df_input.loc[i]
+        #print(type(aSeries), aSeries)
+        #print(type(aSeries), aSeries.values.tolist())
+        
+        record_summarized = [geoid]
+        record_normalized = [geoid]
+        for j in range(len(df_codebook)):
+            if (df_codebook.loc[j, 'use'] != 1): continue
+            if isinstance(df_codebook.loc[j, 'CODE'], float): continue
+            codes = df_codebook.loc[j, 'CODE'].split(",")
+            #print(j, codes, df_codebook.loc[j, 'denominator'], type(df_codebook.loc[j, 'denominator']))
+            value = 0
+            isNaN = True
+            for k in range(len(codes)):
+                code = codes[k].strip()
+                if code not in aSeries.index:
+                    print(f"Warning: '{code}' not found in input data. Skipping...")
+                    continue
+                if math.isnan(aSeries.at[code]) or aSeries.at[code] == -666666666:
+                #if (math.isnan(aSeries.at[code]) or aSeries.at[code] == -666666666): 
+                    #print(i, j, k, code, aSeries.at[code], df_codebook.loc[j, 'Original_Name'])
+                    continue
+                value += aSeries.at[code]
+                isNaN = False
+            if (isNaN): value = NO_DATA_VALUE 
+            #print(j, value, codes)
+            record_summarized.append(value)
+            
+            isNaN = True
+            denominator = df_codebook.loc[j, 'denominator']
+            #if (isinstance(denominator, str) and denominator not in aSeries):
+            #    print(j, codes, denominator, "not in " + inputFile)
+            if (isinstance(denominator, str) and denominator in aSeries and 
+                aSeries.at[denominator] != 0 and aSeries.at[denominator] != -666666666):
+                #print(j, codes, denominator, aSeries.at[denominator])
+                isNaN = False
+                value = value / aSeries.at[denominator]*100
+            if (isNaN): value = value
+            record_normalized.append(value)
+            
+        #print(i, record_summarized)
+        list_summarized.append(record_summarized)
+        list_normalized.append(record_normalized)
+    #print(list_summarized)
+    df_summarized = pd.DataFrame(columns=header_summarized, data=list_summarized)
+    df_normalized = pd.DataFrame(columns=header_normalized, data=list_normalized)
+    #print(df_summarized)
+    #print(df_normalized)
+    
+    df_summarized.to_csv(outputFile+"_summarized.csv", index=False)
+    print("outputFile  file is {}".format(outputFile+"_summarized.csv"))
+    df_normalized.to_csv(outputFile+"_normalized.csv", index=False)
+    print("outputFile  file is {}".format(outputFile+"_normalized.csv"))
+    
+    return
+
+ 
+def getParameter(argv):
+    inputFile = ''
+    codebook = ''
+    ouputDir = ''
+    
+    try:
+        opts, args = getopt.getopt(argv, "hi:c:o:", ["inputFile=", "codebook=", "ouputDir="])
+    except getopt.GetoptError:
+        print("ACS_selection.py -i <inputFile> -c <codebook> -o <ouputDir>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("ACS_selection.py -i <inputFile> -c <codebook> -o <ouputDir>")
+            sys.exit()
+        elif opt in ("-i", "--inputFile"):
+            inputFile = arg
+        elif opt in ("-c", "--codebook"):
+            codebook = arg
+        elif opt in ("-o", "--ouputDir"):
+            ouputDir = arg
+    
+    print("inputFile   file is {}".format(inputFile))
+    print("codebook    file is {}".format(codebook))
+    print("output directory is {}".format(ouputDir))
+    
+    return [inputFile, codebook, ouputDir]
+
+
+if __name__ == '__main__':
+# python ACS_selection_blockgroup.py -i data/ACS_2023_5year__cbg_CA.csv -c data/ACS_2023_5year_codebook_all.xlsx -o data/output_California
+
+# python ACS_selection_blockgroup.py -i data/ACS_2020_5year__block_Texas.csv -c data/ACS_2020_5year_codebook_all.xlsx -o data/output_Texas
+# python ACS_selection_tract.py -i data/ACS_2019_5year__tract_Texas.csv -c data/ACS_2019_5year_codebook_all.xlsx -o data/output_Texas
+# python ACS_selection_tract.py -i data/ACS_2018_5year__tract_IL.csv -c data/ACS_2018_5year_codebook_all.xlsx -o data/output_tract_IL/all
+# python ACS_selection_tract.py -i data/ACS_2018_5year__tract_IL.csv -c data/ACS_2018_5year_codebook.xlsx -o data/output/20variables
+# python ACS_selection_tract.py -i data/ACS_2018_5year__tract_IL.csv -c data/ACS_2018_5year_codebook_all_for_VNE.xlsx -o data/output_tract/all
+
+    started_datetime = datetime.now()
+    print('ACS_selection start at {}'.format(started_datetime.strftime('%Y-%m-%d %H:%M:%S')))
+    
+    # Get parameter from console
+    parameter = getParameter(sys.argv[1:])
+    inputFile = parameter[0]
+    codebook  = parameter[1]
+    ouputDir  = parameter[2]
+    
+    main(inputFile, codebook, ouputDir)
+    
+    ended_datetime = datetime.now()
+    elapsed = ended_datetime - started_datetime
+    total_seconds = int(elapsed.total_seconds())
+    hours, remainder = divmod(total_seconds,60*60)
+    minutes, seconds = divmod(remainder,60)	
+    print('ACS_selection ended at {}    Elapsed {:02d}:{:02d}:{:02d}'.format(ended_datetime.strftime('%Y-%m-%d %H:%M:%S'), hours, minutes, seconds))
